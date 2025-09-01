7d6ca7c65732b743f1e624a9ff8a74004f4a1b07
Ricardo1703-svg <ralvares961@gmail.com>
2025-09-01 15:32:05 -0600

    se cambio para el parcial

---
+++ script.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Codetech",
+    "url": "https://ejemplo.com",
+    "logo": "https://ejemplo.com/logo.png",
+    "sameAs": [
+    "https://facebook.com/codetech",
+    "https://www.linkedin.com/company/codetech"
+    ],
+    "address": {
+        "@type": "PostalAddress",
+    "addressLocality": "San Miguel",
+    "addressRegion": "SV",
+    "addressCountry": "SV"
+    },
+    "contactPoint": {
+        "@type": "ContactPoint",
+    "contactType": "Ventas",
+    "telephone": "+503 7000 0000",
+    "email": "hola@codetech.com"
+    }
+  }
+</script>
